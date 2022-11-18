ccbed2b40b5763d5edaa6a42ad67d9977fce9dc1
udaykirankolla <uday7702192162@gmail.com>
2022-11-18 10:44:51 -0600

    added

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var Door = require("./models/door");
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }));
+  app.use(require('express-session')({ 
+    secret: 'keyboard cat', 
+    resave: false, 
+    saveUninitialized: false 
+  })); 
+  app.use(passport.initialize()); 
+  app.use(passport.session()); 
+  var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+  
 
 
 
